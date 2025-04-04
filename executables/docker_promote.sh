595f578445b55aca4c896106f6fd08f6e2176484
Vincent Boutour <bob@vibioh.fr>
2025-04-04 13:35:41 +0200

    refactor(promote): Using registry v2 api for promoting
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- executables/docker_promote.sh
+++ executables/docker_promote.sh
@@ -30,60 +30,16 @@ dockerhub_auth() {
 
   if [[ ${HTTP_STATUS} != "200" ]]; then
     http_handle_error "Unable to retrieve token for ${1}"
-    return 1
+    exit 1
   fi
 
   HTTP_CLIENT_ARGS+=("--header" "Authorization: Bearer $(jq --raw-output .token "${HTTP_OUTPUT}")")
   rm "${HTTP_OUTPUT}"
 }
 
-main() {
-  source "$(script_dir)/meta" && meta_check "var" "http"
-
-  local DATE_VERSION
-
-  OPTIND=0
-
-  while getopts ":d:" option; do
-    case "${option}" in
-    d)
-      DATE_VERSION="${OPTARG}"
-      ;;
-    :)
-      printf -- "option -%s requires a value\n" "${OPTARG}" >&2
-      return 1
-      ;;
-    \?)
-      printf -- "option -%s is invalid\n" "${OPTARG}" >&2
-      return 2
-      ;;
-    esac
-  done
-
-  shift $((OPTIND - 1))
-
-  if [[ ${#} -lt 2 ]]; then
-    var_red "Usage: ${0} DOCKER_IMAGE IMAGE_VERSION [VERSION_TARGET]"
-    return 1
-  fi
-
-  local DOCKER_IMAGE="${1}"
-  shift
-  local IMAGE_VERSION="${1}"
-  shift
-  local VERSION_TARGET="${1:-latest}"
-  shift || true
-
-  http_init_client
-
-  var_read DOCKER_REGISTRY "https://registry-1.docker.io/v2"
-  if [[ ${DOCKER_REGISTRY} == "https://registry-1.docker.io/v2" ]]; then
-    dockerhub_auth "${DOCKER_IMAGE}"
-  fi
-
-  var_info "Tagging image ${DOCKER_IMAGE} from ${IMAGE_VERSION} to ${VERSION_TARGET}..."
-
+dockerhub_promote() {
   local MANIFEST_ACCEPT=("application/vnd.oci.image.index.v1+json" "application/vnd.docker.distribution.manifest.list.v2+json" "application/vnd.docker.distribution.manifest.v2+json")
+
   for manifest in "${MANIFEST_ACCEPT[@]}"; do
     var_warning "Trying with manifest ${manifest}"
 
@@ -126,9 +82,93 @@ main() {
 
     return
   done
+}
 
-  var_error "Manifest not found!"
-  return 1
+registry_promote() {
+  # Getting manifest
+  http_request --request GET "${DOCKER_REGISTRY}/${DOCKER_IMAGE}/manifests/${IMAGE_VERSION}"
+  if [[ ${HTTP_STATUS} != "200" ]]; then
+    http_handle_error "Unable to retrieve manifest for ${DOCKER_REGISTRY}/${DOCKER_IMAGE}/manifests/${IMAGE_VERSION}"
+    exit 1
+  fi
+
+  local MANIFEST_PAYLOAD
+  MANIFEST_PAYLOAD="$(cat "${HTTP_OUTPUT}")"
+  rm "${HTTP_OUTPUT}"
+
+  # Promoting image
+  http_request --request PUT "${DOCKER_REGISTRY}/${DOCKER_IMAGE}/manifests/${VERSION_TARGET}" \
+    --data "${MANIFEST_PAYLOAD}"
+  if [[ ${HTTP_STATUS} != "201" ]]; then
+    http_handle_error "Unable to promote manifest for ${DOCKER_REGISTRY}/${DOCKER_IMAGE}/manifests/${VERSION_TARGET}"
+    exit 1
+  fi
+
+  var_success "Image promoted to ${VERSION_TARGET}!"
+  rm "${HTTP_OUTPUT}"
+
+  if [[ -n ${DATE_VERSION-} ]]; then
+    # Tagging image with timestamp
+    http_request --request PUT "${DOCKER_REGISTRY}/${DOCKER_IMAGE}/manifests/${DATE_VERSION}" \
+      --data "${MANIFEST_PAYLOAD}"
+    if [[ ${HTTP_STATUS} != "201" ]]; then
+      http_handle_error "Unable to tag date manifest for ${DOCKER_REGISTRY}/${DOCKER_IMAGE}/manifests/${DATE_VERSION}"
+      exit 1
+    fi
+
+    var_success "Image tagged ${DATE_VERSION}!"
+    rm "${HTTP_OUTPUT}"
+  fi
+}
+
+main() {
+  source "$(script_dir)/meta" && meta_check "var" "http"
+
+  local DATE_VERSION
+
+  OPTIND=0
+
+  while getopts ":d:" option; do
+    case "${option}" in
+    d)
+      DATE_VERSION="${OPTARG}"
+      ;;
+    :)
+      printf -- "option -%s requires a value\n" "${OPTARG}" >&2
+      exit 1
+      ;;
+    \?)
+      printf -- "option -%s is invalid\n" "${OPTARG}" >&2
+      return 2
+      ;;
+    esac
+  done
+
+  shift $((OPTIND - 1))
+
+  if [[ ${#} -lt 2 ]]; then
+    var_red "Usage: ${0} DOCKER_IMAGE IMAGE_VERSION [VERSION_TARGET]"
+    exit 1
+  fi
+
+  local DOCKER_IMAGE="${1}"
+  shift
+  local IMAGE_VERSION="${1}"
+  shift
+  local VERSION_TARGET="${1:-latest}"
+  shift || true
+
+  http_init_client
+
+  var_info "Tagging image ${DOCKER_IMAGE} from ${IMAGE_VERSION} to ${VERSION_TARGET}..."
+
+  var_read DOCKER_REGISTRY "https://registry-1.docker.io/v2"
+  if [[ ${DOCKER_REGISTRY} == "https://registry-1.docker.io/v2" ]]; then
+    dockerhub_auth "${DOCKER_IMAGE}"
+    dockerhub_promote
+  else
+    registry_promote
+  fi
 }
 
 main "${@}"
